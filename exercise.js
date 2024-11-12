d14742c087a1653faeadec0e2f925eadd0ea2849
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-11-12 15:30:12 +0500

    Add files via upload

--- exercise.js
+++ exercise.js
@@ -1,20 +1,31 @@
-const formatedate = (date = new Date())=>{
-    const day = date.getDate() ;
-    const months = date.getMonth()+1;
-    const year = date.getFullYear();
-    return `${day}/${months}/${year}`
-}
+const closeto100 = (a,b) => (100 - a) < (100 - b) ? a : b;
 
-console.log(formatedate())
+console.log(closeto100(10,20))
+console.log(closeto100(30,60))
+console.log(closeto100(39,20))
 
 
-// que 5
+
+// // que2 
+
+// const countchars = (str, char) =>
+//     str.split("").filter(ch => ch === char).length;
+// const contains2to4 = (str, char) =>
+//     countchars(str, char) >= 2 && countchars(str, char) <=4;
 
 
-const addnew = (str)=>
-    str.indexOf("!new") ===0 ? str : `!new ${str}`
 
 
-console.log(addnew(" usama "))
 
 
+//how many evven number
+
+const countEvennumber = (arr) =>
+    arr.filter(num => num % 2 === 0).length
+console.log(countEvennumber([2,3,4,5,6,7,8,9,0,12,45,67,896,544,67]));
+
+
+
+const printEvennumber = (arr) =>
+    arr.filter(num => num % 2 === 0)
+console.log(printEvennumber([1,2,3,4,5,6,7,8,9,0,12,43,56,76,45,34,44,334,5,6,7,77,8,88,]));
