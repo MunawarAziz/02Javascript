4513f2ce07abbcab8c3fc980746696ae65da992d
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-11-13 14:45:51 +0500

    Add files via upload

---
+++ 2nd.js
@@ -0,0 +1,8 @@
+ const isleapyear = (year) => year % 4 === 0;
+
+ console.log(isleapyear(2024));
+ console.log(isleapyear(2026));
+ console.log(isleapyear(2001));
+ console.log(isleapyear(2000));
+ console.log(isleapyear(2029));
+ 
